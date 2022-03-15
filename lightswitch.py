4dce297c9e33d9880f4c37bc77fc34c81ff28b55
HamzaHammouti <hamzahhammouti@gmail.com>
2022-03-15 11:46:18 +0100

    Light switch werkend en afgemaakt

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,30 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
+button = tk.Button(text='Lichtknop', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
+knop = 1
 
+def click(event):
+    global knop
+    if knop == 1:
+        window.configure(bg = "yellow")
+        button['text'] = 'Switch light on'
+        print("Switch light on")
+        knop = 2
+    elif knop == 2:
+        
+        window.configure(bg = "black")
+        button['text'] = 'Switch light off'
+        print("Switch light off")
+        knop = 1
 
+#-----------------------------
 
+button.bind('<Button-1>', click)   
+        
 # schijf hier tussen je code
 
 window.mainloop()
